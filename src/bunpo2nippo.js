4160376e96b88949c545e131fa632758c0e30ff5
Koutarou Chikuba <miz404@gmail.com>
2015-11-26 21:04:51 +0900

    Parse

--- src/bunpo2nippo.js
+++ src/bunpo2nippo.js
@@ -1,6 +1,7 @@
 const moment = require('moment');
 const fs = require('fs')
 const path = require('path')
+const groupBy = require('lodash.groupby');
 
 const Slack = require('slack-node');
 const configPath = path.join(process.env.HOME, '.bunpo2nipporc');
@@ -45,6 +46,72 @@ function fetchMessagesOfToday(channelId, userId) {
   });
 }
 
+const typeRegex = new RegExp("^([a-zA-Z-]+)(?!://):\s*");
+function classifyMessagesByTag(messages) {
+  return groupBy(messages, m => {
+    const ret = m.text.match(typeRegex);
+    return ret && ret[1];
+  });
+}
+
+function processMessages(groupedMmessages) {
+  const tasks = {};
+  const uncategorized = [];
+  const others = {};
+
+  for (let type in groupedMmessages) {
+    const typeMessages = groupedMmessages[type];
+
+    if (type === 'task') {
+      for (let m of typeMessages) {
+        const raw = m.text.replace(typeRegex, '');
+        tasks[raw] = false;
+      }
+    } else if (type === 'done') {
+      for (let m of typeMessages) {
+        const raw = m.text.replace(typeRegex, '');
+        tasks[raw] = true;
+      }
+    } else if (type !== 'null'){
+      for (let m of typeMessages) {
+        if (!others[type]) others[type] = [];
+        others[type].push(m);
+      }
+    } else {
+      for (let m of typeMessages) {
+        uncategorized.push(m);
+      }
+    }
+  }
+  // TODO
+  // console.log('tasks', tasks);
+  // console.log('uncategorized', uncategorized)
+
+  console.log('## やったこと\n');
+  for (let task in tasks) {
+    const done = tasks[task];
+    console.log(`${done ? '- [x]' : '- [ ]'}` + task)
+  }
+  console.log('');
+
+  for (let key in others) {
+    // console.log('others', key);
+    const messages = others[key];
+    console.log('##', key, '\n');
+    for (let m of messages) {
+      const raw = m.text.replace(typeRegex, '');
+      console.log(`-${raw}`);
+    }
+    console.log('');
+  }
+
+  console.log('');
+  console.log('## その他\n');
+  for (let m of uncategorized) {
+    console.log(`- ${m.text}`)
+  }
+}
+
 export async function run(config = {}) {
   if (!channelId || !userId) {
     const {user, channel} = await fetchUserAndChannel();
@@ -55,7 +122,9 @@ export async function run(config = {}) {
     }, null, '  '));
   }
   const messages = await fetchMessagesOfToday(channelId, userId);
-  console.log(messages.map(m => {
-    return (moment.unix(parseInt(m.ts, 10)).format('a hh:mm ') + m.text);
-  }).join('\n'));
+  const processdeMessages = processMessages(classifyMessagesByTag(messages));
+
+  // console.log(messages.map(m => {
+  //   return (moment.unix(parseInt(m.ts, 10)).format('a hh:mm ') + m.text);
+  // }).join('\n'));
 };
